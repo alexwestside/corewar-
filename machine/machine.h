8949f85fd974b98ecb25abd17be8415ab03e38fe
Andriy Yatsynyak <andriy.aytsynyak@gmail.com>
2017-06-15 22:40:36 +0300

    init structurs

--- machine/machine.h
+++ machine/machine.h
@@ -16,11 +16,36 @@
 # include "../libft/ft_printf.h"
 # include "../op.h"
 
-typedef struct	s_process
+typedef struct	s_fork
 {
 	int			carry : 1; 			// carry flag
+	int 		live : 1;			// process worker
 	size_t		pc;           	 	// position in char elements
 	int			reg[REG_NUMBER];	// registers of the processor
-}				t_process;
+}				t_fork;
+
+typedef struct s_list_forks
+{
+	t_fork		fork;
+	t_fork		*prev;
+	t_fork		*next;
+}				t_list_forks;
+
+typedef struct	s_query
+{
+	t_fork		*p_process;
+	int			time_cycle;
+
+}				t_query;
+
+typedef	struct	s_machine
+{
+	t_fork		*head;
+	t_fork		*tail;
+	unsigned	count_live;
+	t_query		*top;
+	t_query		*buttom;
+	header_t	*player;
+}				t_machine;
 
 #endif
