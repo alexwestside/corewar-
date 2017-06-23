0afad0caf8b2d3b2325577908298cfeceb13f0da
Andriy Yatsynyak <andriy.aytsynyak@gmail.com>
2017-06-23 23:36:39 +0300

    change print and add func

--- machine/print.c
+++ machine/print.c
@@ -1,15 +1,56 @@
 
 #include "machine.h"
 
+
+char		*cust_strtrim(char const *s, size_t size)
+{
+	char	*f;
+	char	*new;
+	char	*copy_s;
+
+	new = (char *)ft_memalloc(size);
+	if (!s || !new)
+		return (NULL);
+	ft_memcpy(new, s, size);
+	f = new;
+	s = new;
+	f += size;
+	while (ft_isspace(*s) || (*s == '\0'))
+		s++;
+	while (ft_isspace(*f) || (*f == '\0'))
+		f--;
+	*++f = '\0';
+	copy_s = ft_strdup(s);
+	free(new);
+	return (copy_s);
+}
+
+void	format_player(char *fmt_st, char *fmt_end, const char *str, size_t size)
+{
+	size_t	i;
+	char 	*s;
+
+	i = -1;
+	ft_putstr(fmt_st);
+	s = cust_strtrim(str, size);
+	while (++i < size + 1)
+		str[i] ? ft_putchar(str[i]) : 0;
+	ft_putstr(fmt_end);
+	free(s);
+}
+
 void	head_print(t_machine vm)
 {
 	unsigned i;
 
 	i = -1;
 	ft_printf("Introducing contestants...\n");
-	while ( ++i < vm.count_players)
-		ft_printf("%c Player %u, weighing %u, \"%s\" (\"%s\") !\n", '*',  i + 1,
-				  vm.players[i].prog_size, vm.players[i].prog_name,
-				  vm.players[i].comment);
+	while (++i < vm.count_players)
+	{
+		ft_printf("%c Player %u, weighing %u bytes, ", '*', i + 1, vm.players[i].prog_size);
+		format_player("\"", "\"", vm.players[i].prog_name, PROG_NAME_LENGTH);
+		ft_putchar(' ');
+		format_player("(\"", "\")", vm.players[i].comment, COMMENT_LENGTH);
+		ft_putstr(" !\n");
+	}
 }
-
