010cf30ec35552d9f365274d16b5960e1257959a
Wismut <mivanov@unit.student.ua>
2017-07-08 14:39:48 +0300

    upd usage

--- machine/usage.c
+++ machine/usage.c
@@ -4,7 +4,16 @@ void usage(int count, char *s)
 {
 	if (count == 1)
 	{
-		ft_putstr("Usage: ./corewar [-dump nbr_cycles] [[-n number] champion1.cor] ...");
+		ft_putstr("Usage: ./corewar [-dump nbr_cycles] [[-n number] champion1.cor] ...\n");
 		exit(0);
 	}
+	else if (count == 2)
+	{
+		ft_putstr("File ");
+		ft_putstr(s);
+		ft_putendl(" is too small to be a champion");
+		exit(0);
+	}
+	else
+		exit(0);
 }
