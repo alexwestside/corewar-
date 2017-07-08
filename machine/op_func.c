f30b103ad585691cdde2c1ca530641f3fc940e43
Andriy Yatsynyak <andriy.aytsynyak@gmail.com>
2017-07-08 04:22:38 +0300

    the basis for the future all funcs into op

--- machine/op_func.c
+++ machine/op_func.c
@@ -26,34 +26,38 @@ void	test_code_octal(t_machine *vm, unsigned char code)
 	ft_putchar('\n');
 }
 
-int 	*code_octal(int code)
-{
-	int bytecode[3];
+//int 	*code_octal(int code)
+//{
+//	int bytecode[3];
+//
+//
+//}
+
+/*
+ *  !!!!! don't delete some func it is important !!!!!!
+ */
 
 
-}
-
-
-void	select_op(t_machine vm, t_forks *task)
-{
-	static void	(*func_select[17])(t_machine *, t_forks *);
-
-	func_select[0] = live;
-	func_select[1] = ld;
-	func_select[2] = st;
-	func_select[3] = add;
-	func_select[4] = sub;
-	func_select[5] = and;
-	func_select[6] = or;
-	func_select[7] = xor;
-	func_select[8] = zjmp;
-	func_select[9] = ldi;
-	func_select[10] = sti;
-	func_select[11] = fork;
-	func_select[12] = lld;
-	func_select[13] = lldi;
-	func_select[14] = lfork;
-	func_select[15] = aff;
-
-	func_select[task->node.pc](vm, task);
-}
+//void	select_op(t_machine vm, t_forks *task)
+//{
+//	static void	(*func_select[17])(t_machine *, t_forks *);
+//
+//	func_select[0] = live;
+//	func_select[1] = ld;
+//	func_select[2] = st;
+//	func_select[3] = add;
+//	func_select[4] = sub;
+//	func_select[5] = and;
+//	func_select[6] = or;
+//	func_select[7] = xor;
+//	func_select[8] = zjmp;
+//	func_select[9] = ldi;
+//	func_select[10] = sti;
+//	func_select[11] = fork;
+//	func_select[12] = lld;
+//	func_select[13] = lldi;
+//	func_select[14] = lfork;
+//	func_select[15] = aff;
+//
+//	func_select[task->node.pc](vm, task);
+//}
