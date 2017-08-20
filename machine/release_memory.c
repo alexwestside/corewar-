e2f65a9062298bb20140c6abfa67494274f2bbf4
Andriy Yatsynyak <ayatsyny@e1r8p15.unit.ua>
2017-08-20 21:48:01 +0300

    norm release_memory.c

--- machine/release_memory.c
+++ machine/release_memory.c
@@ -1,26 +1,35 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   release_memory.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayatsyny <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/20 21:47:14 by ayatsyny          #+#    #+#             */
+/*   Updated: 2017/08/20 21:47:17 by ayatsyny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "machine.h"
 
-
-void    release_memory(t_machine *vm)
+void	release_memory(t_machine *vm)
 {
-    unsigned i;
+	unsigned i;
 
-    i = -1;
-    if (vm == NULL)
-        return ;
-    free(vm->arena);
-    vm->arena = NULL;
-    free(vm->color_arena);
-    vm->color_arena = NULL;
-    all_delete(&vm->head_lst);
-    vm->head_lst = NULL;
-    while (++i < vm->count_players)
-    {
-        free(vm->players[i].code);
-        vm->players[i].code = NULL;
-    }
-    free(vm->players);
-    vm->players = NULL;
+	i = -1;
+	if (vm == NULL)
+		return ;
+	free(vm->arena);
+	vm->arena = NULL;
+	free(vm->color_arena);
+	vm->color_arena = NULL;
+	all_delete(&vm->head_lst);
+	vm->head_lst = NULL;
+	while (++i < vm->count_players)
+	{
+		free(vm->players[i].code);
+		vm->players[i].code = NULL;
+	}
+	free(vm->players);
+	vm->players = NULL;
 }
