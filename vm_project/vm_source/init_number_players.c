a32d80b3650048364e3109cc1e4d8eba09b36908
Maksym Ivanov <mivanov@e3r4p4.unit.ua>
2017-10-07 17:39:04 +0300

    virtual machine norm

--- vm_project/vm_source/init_number_players.c
+++ vm_project/vm_source/init_number_players.c
@@ -12,7 +12,7 @@
 
 #include "machine.h"
 
-static int	check_valid_is_file(char **str, int index, int len_str, int mod)
+static int		check_valid_is_file(char **str, int index, int len_str, int mod)
 {
 	int fd;
 
@@ -29,41 +29,41 @@ static int	check_valid_is_file(char **str, int index, int len_str, int mod)
 **  num doesnt be 0
 */
 
-static void set_query_number(t_machine *vm, int index)
+static void		set_query_number(t_machine *vm, int index)
 {
-    int         j;
-    unsigned    i;
+	int			j;
+	unsigned	i;
 
-    j = 1;
-    while (j <= (int)vm->count_players)
-    {
-        i = -1;
-        while (++i < vm->count_players && j <= (int)vm->count_players)
-            if (vm->players[i].id == j)
-                j++;
-            else
-            {
-                vm->players[index].id = j;
-                return ;
-            }
-    }
+	j = 1;
+	while (j <= (int)vm->count_players)
+	{
+		i = -1;
+		while (++i < vm->count_players && j <= (int)vm->count_players)
+			if (vm->players[i].id == j)
+				j++;
+			else
+			{
+				vm->players[index].id = j;
+				return ;
+			}
+	}
 }
 
-static void	set_number(t_machine *vm, int index, int num)
+static void		set_number(t_machine *vm, int index, int num)
 {
-    unsigned	i;
+	unsigned	i;
 
-    i = -1;
-    if (num > (int)vm->count_players || num <= 0)
-        error_exit(NULL, 10, vm);
-    while (++i < vm->count_players)
-        if (vm->players[i].id == num)
-            error_exit(NULL, 10, vm);
-    if (num && vm->players[index].id == 0)
-        vm->players[index].id = num;
+	i = -1;
+	if (num > (int)vm->count_players || num <= 0)
+		error_exit(NULL, 10, vm);
+	while (++i < vm->count_players)
+		if (vm->players[i].id == num)
+			error_exit(NULL, 10, vm);
+	if (num && vm->players[index].id == 0)
+		vm->players[index].id = num;
 }
 
-void		init_number_players(t_machine *vm, int argc, char **argv)
+void			init_number_players(t_machine *vm, int argc, char **argv)
 {
 	int			num;
 	int			i_arg;
@@ -85,5 +85,5 @@ void		init_number_players(t_machine *vm, int argc, char **argv)
 		}
 		else if (check_valid_is_file(argv, i_arg, argc, 0))
 			if (j_players + 1 < vm->count_players)
-                set_query_number(vm, ++j_players);
+				set_query_number(vm, ++j_players);
 }
